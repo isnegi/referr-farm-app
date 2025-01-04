425907491abe3f2bea0d802c40399e182b31ccaa
isnegi <shubhamnegi.singh882@gmail.com>
2025-01-04 15:12:28 +0530

    updated how it works pagfe

--- app/(default)/how-it-works/page.tsx
+++ app/(default)/how-it-works/page.tsx
@@ -1,6 +1,7 @@
 import VideoIFrame from "@/app/_components/common/video-iframe";
 import { AlarmClock } from "lucide-react";
-import heroImg from '@/public/assets/images/hero-img.png';
+import workflow_1 from '@/public/assets/images/workflow_1.png';
+import workflow_2 from '@/public/assets/images/workflow_2.png';
 import Image from "next/image";
 
 export default function HowItWorks() {
@@ -163,12 +164,13 @@ export default function HowItWorks() {
       {/* BOTTOM SECTION STARTS */}
       <div className="overflow-hidden bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          {/* WORKFLOW 1 STARTS */}
+          <div className="mx-auto my-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 items-center">
             <div className="lg:pr-8 lg:pt-4">
               <div className="lg:max-w-lg">
-                <h2 className="text-base/7 font-semibold text-[#5ae3a9]">Deploy faster</h2>
+                <h2 className="text-base/7 font-semibold text-[#5ae3a9]">Become a hero</h2>
                 <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-                  A better workflow
+                  Earn by Referrals
                 </p>
                 <p className="mt-6 text-lg/8 text-gray-600">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
@@ -188,18 +190,48 @@ export default function HowItWorks() {
               </div>
             </div>
             <Image
-              width={2432}
-              src={heroImg}
+              width={340}
+              height={400}
+              src={workflow_1}
               alt="hero image"
-              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" />
-            {/* <img
-              alt="Product screenshot"
-              src={heroImg}
-              width={2432}
-              height={1442}
-              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-            /> */}
+            />
           </div>
+          {/* WORKFLOW 1 ENDS */}
+
+          {/* WORKFLOW 2 STARTS */}
+          <div className="mx-auto my-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 items-center">
+            <Image
+              width={340}
+              height={400}
+              src={workflow_2}
+              alt="hero image"
+            />
+
+            <div className="lg:pr-8 lg:pt-4">
+              <div className="lg:max-w-lg">
+                <h2 className="text-base/7 font-semibold text-[#5ae3a9]">Job search</h2>
+                <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                  Next big opportunity
+                </p>
+                <p className="mt-6 text-lg/8 text-gray-600">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                  iste dolor cupiditate blanditiis ratione.
+                </p>
+                <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
+                  {features.map((feature) => (
+                    <div key={feature.name} className="relative pl-9">
+                      <dt className="inline font-semibold text-gray-900">
+                        <feature.icon aria-hidden="true" className="absolute left-1 top-1 size-5 text-[#5ae3a9]" />
+                        {feature.name}
+                      </dt>{' '}
+                      <dd className="inline">{feature.description}</dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+            </div>
+          </div>
+          {/* WORKFLOW 2 ENDS */}
         </div>
       </div>
       {/* BOTTOM SECTION ENDS */}
